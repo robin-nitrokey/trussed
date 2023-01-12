69c7b7af13ec2b2d25c4ea05aecae00316f91e1f
Robin Krahl <robin@nitrokey.com>
2023-01-12 22:39:58 +0100

    Pass ServiceResources to ServiceBackend
    This makes it possible to access the platform or call the software
    backend implementation for a request.

--- src/service.rs
+++ src/service.rs
@@ -66,12 +66,20 @@ impl<P: Platform> ServiceResources<P> {
             rng_state: None,
         }
     }
+
+    pub fn platform(&self) -> &P {
+        &self.platform
+    }
+
+    pub fn platform_mut(&mut self) -> &mut P {
+        &mut self.platform
+    }
 }
 
 pub struct Service<P, B>
 where
     P: Platform,
-    B: Backends<P::B>,
+    B: Backends<P>,
 {
     eps: Vec<ServiceEndpoint<P::B, P::I>, { MAX_SERVICE_CLIENTS::USIZE }>,
     resources: ServiceResources<P>,
@@ -79,7 +87,7 @@ where
 }
 
 // need to be able to send crypto service to an interrupt handler
-unsafe impl<P: Platform, B: Backends<P::B>> Send for Service<P, B> {}
+unsafe impl<P: Platform, B: Backends<P>> Send for Service<P, B> {}
 
 impl<P: Platform> ServiceResources<P> {
     #[inline(never)]
@@ -688,7 +696,7 @@ impl<P: Platform> ServiceResources<P> {
     }
 }
 
-impl<P: Platform, B: Backends<P::B>> Service<P, B> {
+impl<P: Platform, B: Backends<P>> Service<P, B> {
     pub fn new(platform: P, backends: B) -> Self {
         let resources = ServiceResources::new(platform);
         Self {
@@ -816,7 +824,8 @@ impl<P: Platform, B: Backends<P::B>> Service<P, B> {
                             }
                             ServiceBackends::Custom(backend) => {
                                 if let Some(backend) = self.backends.select(backend) {
-                                    reply_result = backend.reply_to(&mut ep.client_ctx, &request);
+                                    reply_result =
+                                        backend.reply_to(&mut ep.client_ctx, &request, resources);
                                 }
                             }
                         }
@@ -861,7 +870,7 @@ impl<P: Platform, B: Backends<P::B>> Service<P, B> {
 impl<P, B> crate::client::Syscall for &mut Service<P, B>
 where
     P: Platform,
-    B: Backends<P::B>,
+    B: Backends<P>,
 {
     fn syscall(&mut self) {
         self.process();
@@ -871,7 +880,7 @@ where
 impl<P, B> crate::client::Syscall for Service<P, B>
 where
     P: Platform,
-    B: Backends<P::B>,
+    B: Backends<P>,
 {
     fn syscall(&mut self) {
         self.process();
